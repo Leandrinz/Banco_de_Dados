9b55a69a0e2f9854a5fe4dfc504eb4039c5cdca9
Leandro <lsavio516@gmail.com>
2026-01-24 10:57:08 -0300

    Inserindo Dados na Tabela
    - Tópicos estudados:
       .  Como inserir Dados no Banco de Dados
       . Múltiplos Cadastros
       . DDL e DML

---
+++ Banco_de_Dados_SQL/4. Inserindo_Dados_na_Tabela/Exercicio.sql
@@ -0,0 +1,9 @@
+-- Active: 1769088182955@@localhost@3306@cadastro
+USE cadastro;
+
+INSERT INTO pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+VALUES
+('Kamilly', '2009-03-24', 'F', '75.2', '1.80', 'Brasil')
+
+SELECT * from pessoas
